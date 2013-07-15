5db96c14e352998778cd8923248cd2d628cd36ec
rain zhai <zhaiyu963@gmail.com>
2013-07-15 22:42:18 +0800

    qjs里面移动场景
    在stage下添加一个专门的bg的对象，来做x/y值的变化以实现场景移动的效果

--- js/game.js
+++ js/game.js
@@ -37,7 +37,7 @@
 			//初始化数据
 			this.eventChildren = false;
 			this.jumping = false;
-			this.speedY = this.currentSpeedY = 25;    
+			this.speedY = this.currentSpeedY = 30;    
 			this.originY = this.y;
 	};
 
@@ -51,7 +51,7 @@
 	//松鼠的更新函数，此方法会不断的被quark系统调用而产生跳跃动画。
 	Mushroom.prototype.update = function(){
     if(this.jumping){
-        this.currentSpeedY -= 2;
+        this.currentSpeedY -= 3;
         this.y -= this.currentSpeedY; 
         if(this.originY <= this.y){
             this.y = this.originY;
@@ -62,10 +62,11 @@
 	//=================================================
 	var container,params, timer, stage, context, em, squirrel;
 	container = Q.getDOM("container");
+	container.style.background = "-moz-linear-gradient(top, #00889d, #94d7e1, #58B000)";
 	params = Q.getUrlParams();
 	console.info(params.canvas);
 	if(params.canvas){
-		var canvas = Quark.createDOM("canvas", {width:480, height:320, style:{position:"absolute"}});
+		var canvas = Quark.createDOM("canvas", {width:480, height:320, style:{position:"absolute",background:"-moz-linear-gradient(top, #00889d, #94d7e1, #58B000)"}});
 		container.appendChild(canvas);
 		context = new Quark.CanvasContext({canvas:canvas});
 	}else{
@@ -75,7 +76,15 @@
 	//初始化舞台
 	stage = new Q.Stage({context:context, width:480, height:320, update:function(){
 		frames++;
+		//this.x++;
 	}});
+	var background = new Q.DisplayObjectContainer({width:1000, height:320, x:-1000, y:0,update:function(){
+		this.x+=4;
+	}});
+	stage.addChild(background);
+	var rect1 = new Q.Graphics({width:100, height:100, x:0, y:0});
+	rect1.drawRect(0, 0, 100, 100).lineStyle(1, "#000").beginFill("red").endFill().cache();
+	background.addChild(rect1);
 	//初始化timer并启动
 	timer = new Q.Timer(1000/30);
 	timer.addListener(stage);
@@ -87,10 +96,8 @@
 	em.registerStage(stage, events, true, true);
 	em.register(stage, events,function(e){ mushroom.x = e.pageX- mushroom.width/2;}, true, true);
   //蘑菇实例   
-	var mushroom = new Mushroom({id:"squirrel", x:200, y:160, autoSize:true});
+	var mushroom = new Mushroom({id:"squirrel", x:200, y:200, autoSize:true});
 	stage.addChild(mushroom);
 
 	//为松鼠添加touchend或mouseup事件侦听，控制其跳跃。
 	mushroom.addEventListener(events[0], mushroom.move);
- 
-	//doc.body.add(doc.container); 
